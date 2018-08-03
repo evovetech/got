377a95b30ca7398c872476d758fa02689330bc5c
Layne Penney <layne@evove.tech>
2018-08-03 03:37:34 -0500

    update gogen.sh

--- gogen.sh
+++ gogen.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-pkgs=(
-    "collect/map.go"
-    "collect/set.go"
-    "collect/list.go"
-)
-
-for p in "${pkgs[@]}"; do
-    go generate -v "./${p}"
-done
+go generate -v ./...
